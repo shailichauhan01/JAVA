fd70394043e5a6dcb0bc9cfa07e922567016fc0b
Shaili Chauhan <114276647+shailichauhan01@users.noreply.github.com>
2024-03-10 19:58:50 +0530

    Delete Array/ForEach.java

--- Array/ForEach.java
+++
@@ -1,9 +0,0 @@
-class ForEach{
-    public static void main(String[] args)
-    {
-        String[]fruits={"Apple","Banana","Cherry","Date"};
-        for(String fruit:fruits ){
-            System.out.println(fruit);
-        }
-    }
-}
